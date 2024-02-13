d7a4dcf3ead815fdbd75fd80c751b45cc491c718
Nata Nael <natanael280198@gmail.com>
2024-02-13 14:36:12 +0700

    feat: added postgres author domain and repo

---
+++ internal/author/repository/postgres/postgres_author.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"database/sql"
+
+	"github.com/natanaelrusli/library-api-gin/internal/domain"
+)
+
+type postgresAuthorRepository struct {
+	Conn *sql.DB
+}
+
+func NewPostgresAuthorRepository(conn *sql.DB) domain.AuthorRepository {
+	return &postgresAuthorRepository{
+		Conn: conn,
+	}
+}
+
+func (r *postgresAuthorRepository) GetByID(id int64) (domain.Author, error) {
+	var author domain.Author
+	q := `
+		SELECT * from authors
+		WHERE deleted_at IS NULL
+		AND id = $1;
+	`
+
+	rows := r.Conn.QueryRow(q, id)
+	if rows.Err() != nil {
+		return domain.Author{}, rows.Err()
+	}
+
+	err := rows.Scan(
+		&author.ID,
+		&author.Name,
+		&author.CreatedAt,
+		&author.UpdatedAt,
+	)
+
+	if err != nil {
+		return domain.Author{}, err
+	}
+
+	return author, nil
+}
---
+++ internal/domain/author.go
@@ -0,0 +1,15 @@
+package domain
+
+import "time"
+
+type Author struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+type AuthorRepository interface {
+	GetByID(id int64) (Author, error)
+}
