dd8628082d84c95d5e2cde19226c9f4cebeca3ee
Nata Nael <natanael280198@gmail.com>
2024-02-15 09:41:30 +0700

    feat: added author repository test

---
+++ internal/author/repository/postgres/postgres_author_test.go
@@ -0,0 +1,54 @@
+package postgres
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/natanaelrusli/library-api-gin/internal/domain"
+)
+
+func TestGetByID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error creating mock database: %v", err)
+	}
+
+	defer db.Close()
+
+	repo := &postgresAuthorRepository{Conn: db}
+
+	expectedRows := sqlmock.
+		NewRows([]string{"id", "name", "created_at", "updated_at", "deleted_at"}).
+		AddRow(
+			1, "John Doe", time.Date(2020, time.April, 11, 21, 34, 01, 0, time.UTC), time.Date(2020, time.April, 11, 21, 34, 01, 0, time.UTC), nil,
+		)
+
+	mock.
+		ExpectQuery("SELECT \\* from authors WHERE deleted_at IS NULL AND id = \\$1;").
+		WithArgs(1).
+		WillReturnRows(expectedRows)
+
+	result, err := repo.GetByID(1)
+
+	if err != nil {
+		t.Fatalf("error calling GetByID: %v", err)
+	}
+
+	expectedResult := domain.Author{
+		ID:        1,
+		Name:      "John Doe",
+		CreatedAt: time.Date(2020, time.April, 11, 21, 34, 01, 0, time.UTC),
+		UpdatedAt: time.Date(2020, time.April, 11, 21, 34, 01, 0, time.UTC),
+		DeletedAt: sql.NullTime{},
+	}
+
+	if result != expectedResult {
+		t.Errorf("Expected result %+v, got %+v", expectedResult, result)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("unfulfilled expectations: %s", err)
+	}
+}
