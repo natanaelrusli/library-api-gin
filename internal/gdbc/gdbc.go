ab1d41240281aac938c56724c8bda95f203a57aa
Nata Nael <natanael280198@gmail.com>
2024-02-19 17:06:52 +0700

    feat: added gdbc for transactions Tx

---
+++ internal/gdbc/gdbc.go
@@ -0,0 +1,21 @@
+package gdbc
+
+import "database/sql"
+
+type SqlGdbc interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Prepare(query string) (*sql.Stmt, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Transactioner
+}
+
+type Transactioner interface {
+	// Rollback a transaction
+	Rollback() error
+	// Commit a transaction
+	Commit() error
+	// TxEnd commits a transaction if no errors, otherwise rollback
+	// txFunc is the operations wrapped in a transaction
+	TxEnd(txFunc func() error) error
+}
